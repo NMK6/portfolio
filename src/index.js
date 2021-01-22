c174eec861fdfd4d53b27ddd7292531d8e5d9be7
NMK6 <mkehre@gmail.com>
2021-01-22 10:06:27 +0000

    styled first screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import Search from './js/models/Logo';
-import { elements } from './js/views/base';
+// import Search from './js/models/Logo';
+// import { elements } from './js/views/base';
 import style from './sass/main.scss';
 import * as firstScreenViews from './js/views/firstScreenViews';
 import cooking from './img/cooking__first-screen.png';
-console.log(firstScreenViews);
 firstScreenViews.addFirstLogo();
