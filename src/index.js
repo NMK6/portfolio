792288e0bfb3435ecc983f469d5f78de8394d898
NMK6 <mkehre@gmail.com>
2021-01-25 00:54:31 +0000

    added title and change screens anim promise

--- src/index.js
+++ src/index.js
@@ -6,37 +6,61 @@ import * as utils from './js/views/utils';
 import * as logoViews from './js/views/logoViews';
 import Logo from './js/models/Logo';
 import Canvas from './js/models/Canvas';
-const state = {};
+
 firstScreenViews.addFirstLogo();
 window.addEventListener('DOMContentLoaded', (e) => {
   e.preventDefault();
-  firstScreenViews.addAnimation();
-  secondScreenViews.addSecondScreen();
-  utils.addVisuallyHidden('.first-screen');
+  const state = {};
+  function changeScreens() {
+    return new Promise(function (resolve, reject) {
+      resolve();
+    });
+  }
+  changeScreens()
+    // .then(function () {
+    //   return firstScreenViews.addFirstLogo();
+    // })
+    .then(firstScreenViews.addAnimation)
 
-  utils.removeVisuallyHidden('.second-screen');
+    .then(secondScreenViews.addSecondScreen)
+    .then(function () {
+      return utils.addVisuallyHidden('.first-screen');
+    })
+    .then(function () {
+      return utils.removeVisuallyHidden('.second-screen');
+    })
+    .then(function () {
+      return secondScreenViews.createTitle(
+        document.querySelector('.second-screen')
+      );
+    })
+    .catch();
+  //   firstScreenViews.addAnimation();
+  //   secondScreenViews.addSecondScreen();
+  //   utils.addVisuallyHidden('.first-screen');
+
+  //   utils.removeVisuallyHidden('.second-screen');
+
+  const addLogo = async () => {
+    state.logo = new Logo();
+    await state.logo.getLogoData();
+    state.canvas = new Canvas(
+      150,
+      100,
+      'canvas__logo',
+      70,
+      70,
+      1.1,
+      1.1,
+      18,
+      24,
+      0.3,
+      elements.root,
+      0,
+      0
+    );
+
+    await logoViews.drawLogo(state.logo.result.logo, state.canvas);
+  };
+  addLogo();
 });
-const addLogo = async () => {
-  state.logo = new Logo();
-  await state.logo.getLogoData();
-  state.canvas = new Canvas(
-    150,
-    100,
-    'canvas__logo',
-    70,
-    70,
-    1.1,
-    1.1,
-    18,
-    24,
-    0.3,
-    elements.root,
-    0,
-    0
-  );
-
-  console.log(state.logo.result.logo[0]);
-
-  await logoViews.drawLogo(state.logo.result.logo, state.canvas);
-};
-addLogo();
--- src/js/views/secondScreenViews.js
+++ src/js/views/secondScreenViews.js
@@ -4,3 +4,29 @@ export const addSecondScreen = () => {
   secondScreen.className = 'second-screen visually-hidden';
   elements.root.appendChild(secondScreen);
 };
+export const createTitle = (parent) => {
+  const title = document.createElement('h1');
+  title.className = 'second-screen__title';
+  parent.appendChild(title);
+  const titleText = 'Front end developer';
+  const titleTextArr = titleText.split('');
+  function showTitle() {
+    if (titleTextArr.length > 0) {
+      const spanLetter = document.createElement('span');
+
+      spanLetter.textContent = titleTextArr.shift();
+      if (spanLetter.textContent != ' ') {
+        spanLetter.classList.add('second-screen__letter');
+      } else {
+        spanLetter.classList.add('second-screen__space');
+      }
+      title.appendChild(spanLetter);
+      setTimeout(function () {
+        showTitle();
+      }, 150);
+    } else {
+      return;
+    }
+  }
+  showTitle();
+};
